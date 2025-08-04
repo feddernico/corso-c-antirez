f8be16ac4677dda460f806b2ace3a243598ddae9
Federico Viscioletti <federico.viscioletti@gmail.com>
2025-08-04 11:33:03 +0100

    Create loop.c
    added a recursive function to count between start and end

---
+++ lesson-6/loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// crea un loop usando if e ricorsione
+void conta(int start, int end) {
+    if (start > end) return;
+    printf("%d\n", start);
+    conta(start+1, end);
+}
+
+int main(void) {
+    conta(0, 9);
+    return 0;
+}
